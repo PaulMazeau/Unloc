e238c3907995777bf7790ce7e4c2077491b35da2
Paul MAZEAU <mazeau.paul@gmail.com>
2023-10-12 21:04:58 +0200

    Début

---
+++ components/navigation/MainStack.jsx
@@ -0,0 +1,15 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react'
+import HomeScreen from '../../screens/Home';
+import ResultScreen from '../../screens/Result';
+
+export default function MainStackComponent() {
+    const MainNavigation = createNativeStackNavigator();
+    
+    return (
+        <MainNavigation.Navigator initialRouteName="Accueil" screenOptions={{ headerShown: false }}>
+            <MainNavigation.Screen name="HomeScreen" component={HomeScreen} />
+            <MainNavigation.Screen name="Details" component={ResultScreen} />
+        </MainNavigation.Navigator>
+    )
+}
---
+++ screens/Result.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function ResultScreen() {
+    return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Page resultat de recherche</Text>
+      </View>
+    )
+}
